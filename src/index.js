5572e4cba5894542372771af06825a58dea7c1cc
SnzDev <tms.working@gmail.com>
2022-01-02 11:09:11 -0300

    feat(deposit):Added insert statement✨

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ app.use(express.json());
 
 const customers = [];
 
+//MIDDLEWARES
+function verifyIfCustomerExists(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(400).json({ error: 'Customer not found' });
+    }
+    request.customer = customer;
+
+    return next();
+}
+
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body;
     const id = uuidv4();
@@ -19,20 +33,31 @@ app.post('/account', (request, response) => {
         cpf,
         name,
         id: uuidv4(),
+        statement: []
     })
     return response.status(201).send();
 });
 
-app.get("/statement/:cpf", (request, response) => {
-    const { cpf } = request.params;
-
-    const customer = customers.find(customer => customer.cpf === cpf);
-
-    if (!customer) { 
-        return response.status(400).json({error: 'Customer not found'});
-    }
+app.get("/statement", verifyIfCustomerExists, (request, response) => {
+    const { customer } = request;
 
     return response.json(customer.statement);
 });
+
+app.post("/deposit", verifyIfCustomerExists, (request, response) => {
+    const { description, amount } = request.body;
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        date: new Date(),
+        operation: "credit",
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send()
+})
 
 app.listen(3333);
